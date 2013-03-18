97e1c1316f31e5c92c82dc775f8810fca0e51570
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-03-17 22:11:00 -0600

    Agregando config

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'beautywp14');
+define('DB_NAME', 'luxspa');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '542596');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
