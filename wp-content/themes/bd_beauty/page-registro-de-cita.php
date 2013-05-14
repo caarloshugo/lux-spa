245f99300c7f6dc1f29c968a9d1cf63cacdb526e
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-05-13 22:23:15 -0500

    ajax

--- wp-content/themes/bd_beauty/page-registro-de-cita.php
+++ wp-content/themes/bd_beauty/page-registro-de-cita.php
@@ -82,7 +82,7 @@ if(isset($_POST["submit"])) {
 		<span class="spinner"><span>Enviando ...</span></span>
 	</p>
 </form>
-
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script type="text/javascript">
 	$(document).ready(function() {
 		$("#tratamiento").change(function() {
