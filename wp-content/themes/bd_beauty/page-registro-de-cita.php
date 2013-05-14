c02181ca0ae3d8167d41962d1e6258fd53f41703
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-05-13 22:27:56 -0500

    ajax

--- wp-content/themes/bd_beauty/page-registro-de-cita.php
+++ wp-content/themes/bd_beauty/page-registro-de-cita.php
@@ -1,4 +1,5 @@
 <?php
+
 // User
 include "class/user.php";
 include "class/functions/string.php";
@@ -7,98 +8,108 @@ $User = new User();
 $therapists  = $User->getTherapists();
 $specialties = $User->getSpecialties();
 
-if(isset($_POST["submit"])) {
-	$name      = $_POST["cf_name"];
-	$lastname  = $_POST["lastname"];
-	$birthday  = $_POST["birthday"];
-	$email     = $_POST["email"];
-	$sex       = $_POST["sex"];
-	$telephone = $_POST["telephone"];
-	
-	if($name !== "" and $lastname !== "" and $email !== "" and $birthday !== "" and $telephone !== "") {
+
+if(isset($_GET["id_t"])) {
+	echo "hello world";
+} else {
 		
-
-		$User = new User();
-		$data = $User->add($name, $lastname, $email, $birthday, $sex, $telephone);
+	if(isset($_POST["submit"])) {
+		$name      = $_POST["cf_name"];
+		$lastname  = $_POST["lastname"];
+		$birthday  = $_POST["birthday"];
+		$email     = $_POST["email"];
+		$sex       = $_POST["sex"];
+		$telephone = $_POST["telephone"];
 		
-		echo '<input type="text" id="success_msg" name="cf_success_msg" value="Thank you for your email we will fixed appointment by phone call" />';
-	} else {
-		echo '<input type="text" id="error_msg" name="cf_error_msg" value="Please fill in all fields correctly" />';
-	}
-	
-} else {	
-	get_header();
-?>
-
-<form id="contactform" class="three-column-form" method="post" action="">
-	<input type="hidden" id="receiver" name="cf_receiver" value="info[at]bdtheme.com" />
-	<input type="hidden" id="email_signature" name="cf_email_signature" value="Appointment Booking" />
-
-
-	<p class="one-third">
-		<label for="email">E-Mail*:</label>
-		<input id="email" name="email" class="required" type="text" />
-	</p>
-	
-	
-	<p class="one-third">
-		<label for="password">Password*:</label>
-		<input id="password" name="password" class="required" type="password" />
-	</p>
-		
-	
-	<p class="one-third last">
-		<label for="date">Fecha de la cita*:</label>
-		<input id="date" name="date" class="required" type="text" />
-	</p>
-	
-	<p class="one-third">
-		<label for="tratamiento">Tratamiento*:</label>
-		<select name="tratamiento" id="tratamiento" class="required">
-			<option value="0">Seleccione un tratamiento</option>
-			<?php foreach($specialties as $specialty) { ?>
-				<option value="<?php echo $specialty["id"];?>">
-					<?php echo ucfirst($specialty["name"]);?>
-				</option>
-			<?php } ?>
-		</select>
-	</p>
-	
-	<p class="one-third">
-		<label for="terapeuta">Terapeuta*:</label>
-		<select name="terapeuta" id="terapeuta" class="required">
-			<?php foreach($therapists as $therapist) { ?>
-				<option value="<?php echo $therapist["id"];?>">
-					<?php echo ucfirst($therapist["name"]);?>
-				</option>
-			<?php } ?>
-		</select>
-	</p>
-	
-	<div class="message"></div>
-
-	<p>
-		<input type="submit" name="submit" value="Registrar" />
-		<span class="spinner"><span>Enviando ...</span></span>
-	</p>
-</form>
-<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-<script type="text/javascript">
-	$(document).ready(function() {
-		$("#tratamiento").change(function() {
-			var str = $("#tratamiento option:selected").text();
-			console.log(str);
+		if($name !== "" and $lastname !== "" and $email !== "" and $birthday !== "" and $telephone !== "") {
 			
-			$.ajax({
-			url: "?page_id=1223",
-				context: document.body
-			}).done(function() {
-				$(this).addClass("done");
+
+			$User = new User();
+			$data = $User->add($name, $lastname, $email, $birthday, $sex, $telephone);
+			
+			echo '<input type="text" id="success_msg" name="cf_success_msg" value="Thank you for your email we will fixed appointment by phone call" />';
+		} else {
+			echo '<input type="text" id="error_msg" name="cf_error_msg" value="Please fill in all fields correctly" />';
+		}
+		
+	} else {	
+		get_header();
+	?>
+
+	<form id="contactform" class="three-column-form" method="post" action="">
+		<input type="hidden" id="receiver" name="cf_receiver" value="info[at]bdtheme.com" />
+		<input type="hidden" id="email_signature" name="cf_email_signature" value="Appointment Booking" />
+
+
+		<p class="one-third">
+			<label for="email">E-Mail*:</label>
+			<input id="email" name="email" class="required" type="text" />
+		</p>
+		
+		
+		<p class="one-third">
+			<label for="password">Password*:</label>
+			<input id="password" name="password" class="required" type="password" />
+		</p>
+			
+		
+		<p class="one-third last">
+			<label for="date">Fecha de la cita*:</label>
+			<input id="date" name="date" class="required" type="text" />
+		</p>
+		
+		<p class="one-third">
+			<label for="tratamiento">Tratamiento*:</label>
+			<select name="tratamiento" id="tratamiento" class="required">
+				<option value="0">Seleccione un tratamiento</option>
+				<?php foreach($specialties as $specialty) { ?>
+					<option value="<?php echo $specialty["id"];?>">
+						<?php echo ucfirst($specialty["name"]);?>
+					</option>
+				<?php } ?>
+			</select>
+		</p>
+		
+		<p class="one-third">
+			<label for="terapeuta">Terapeuta*:</label>
+			<select name="terapeuta" id="terapeuta" class="required">
+				<?php foreach($therapists as $therapist) { ?>
+					<option value="<?php echo $therapist["id"];?>">
+						<?php echo ucfirst($therapist["name"]);?>
+					</option>
+				<?php } ?>
+			</select>
+		</p>
+		
+		<div class="message"></div>
+
+		<p>
+			<input type="submit" name="submit" value="Registrar" />
+			<span class="spinner"><span>Enviando ...</span></span>
+		</p>
+	</form>
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$("#tratamiento").change(function() {
+				var str = $("#tratamiento option:selected").val();
+				console.log(str);
+				
+				if(str=='0') {
+					
+				} else {
+					$.ajax({
+					url: "?page_id=1223?id_t=" + str,
+						context: document.body
+					}).done(function(txt) {
+						console.log(txt);
+					});
+				}
 			});
 		});
-	});
-</script>      
-<?php 
-	get_footer(); 
-}
-?>
+	</script>      
+	<?php 
+		get_footer(); 
+	}
+	?>
+<?php } ?>
